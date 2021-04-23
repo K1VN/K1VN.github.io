1dcdef84dcd6cadc8564c4aac07643cb78c4e5d5
Kevin Ma <kevinma@Kevins-MacBook-Pro.local>
2021-04-23 13:15:06 -0400

    Created GOOK blog website (beta)

---
+++ blog.js
@@ -0,0 +1,7 @@
+function open_menu() {
+    document.getElementById('navbar_list_hidden').style.display = 'block';
+}
+
+function close_menu() {
+    document.getElementById('navbar_list_hidden').style.display = 'none';
+}
